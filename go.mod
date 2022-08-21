6a4fe2a341785fd2e4bcd65fefe28b1179f2b257
Nikitaah! <moniquenegreiros131@gmail.com>
2022-08-21 00:34:13 -0300

    Setup da conexão com o banco e mocks de teste

--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/confluentinc/confluent-kafka-go v1.7.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/joho/godotenv v1.3.0 // indirect
-	github.com/lib/pq v1.10.2 // indirect
+	github.com/lib/pq v1.10.2
 	github.com/satori/go.uuid v1.2.0
 	google.golang.org/grpc v1.39.0 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
--- main.go
+++ main.go
@@ -1,10 +1,56 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"github.com/codeedu/codebank/domain"
+	"github.com/codeedu/codebank/infrastructure/repository"
+	"github.com/codeedu/codebank/usecase"
+	_ "github.com/lib/pq"
+	"log"
 	"moul.io/banner"
 )
 
 func main() {
 	fmt.Println(banner.Inline("codebank"))
+	db := setupDb()
+	defer db.Close()
+
+	//TESTE
+	creditCard := domain.NewCreditCard()
+	creditCard.Number = "1234"
+	creditCard.Name = "Nikita"
+	creditCard.ExpirationYear = 2027
+	creditCard.ExpirationMonth = 12
+	creditCard.CVV = 123
+	creditCard.Limit = 1000
+	creditCard.Balance = 0
+
+	repo := repository.NewTransactionRepositoryDb(db)
+	err := repo.CreateCreditCard(*creditCard)
+	if err != nil {
+		fmt.Println(err)
+	}
+	//TESTE
+}
+
+func setupTransactionUseCase(db *sql.DB) usecase.UseCaseTransaction {
+	transactionRepository := repository.NewTransactionRepositoryDb(db)
+	usecase := usecase.NewUseCaseTransaction(transactionRepository)
+	return usecase
+}
+
+func setupDb() *sql.DB {
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		"db",
+		"5432",
+		"postgres",
+		"root",
+		"codebank",
+	)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatal("error connecting to database")
+	}
+	return db
 }
